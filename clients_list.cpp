d432a8b23ffc3333775189b6e092742bebc26220
msadowsk <01178589@pw.edu.pl>
2023-05-17 20:54:17 +0200

    changed to shared pointers

--- clients_list.cpp
+++ clients_list.cpp
@@ -2,17 +2,22 @@
 #include "client_not_found_exception.h"
 #include "already_existing_client_exception.h"
 
-void ClientsList::add_client(unsigned int id, std::string name, std::string surname, Purpose purpose)
+void ClientsList::add_client(Client client)
 {
     for(const auto& client_ptr : clients)
     {
-        if(client_ptr -> get_id() == id)
+        if(client_ptr -> get_id() == client.get_id())
         {
-            throw AlreadyExistingClientException(id);
+            throw AlreadyExistingClientException(client.get_id());
         }
     }
-    std::unique_ptr<Client> added_client = std::make_unique<Client>(id, name, surname, purpose);
+    std::shared_ptr<Client> added_client = std::make_shared<Client>(client);
     clients.push_back(std::move(added_client));
+}
+
+std::list<std::shared_ptr<Client>> ClientsList::get_clients()
+{
+    return clients;
 }
 
 Client ClientsList::find_client_by_id(unsigned int id) const
