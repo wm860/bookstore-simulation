9544d910ef422db15e01d7c6a44b0cec490a6e4d
msadowsk <01178589@pw.edu.pl>
2023-05-17 20:55:51 +0200

    added == operator to seller

--- seller.cpp
+++ seller.cpp
@@ -2,41 +2,50 @@
 
 Seller::Seller(){}
 
-Seller::Seller(unsigned int id, std::string name, std::string surname, Accessibility accessibility) : 
+Seller::Seller(unsigned int id, std::string name, std::string surname, Accessibility accessibility) :
 id(id), name(name), surname(surname), accessibility(accessibility)
 {}
 
-unsigned int Seller::get_id() const
+unsigned int Seller::get_id() const noexcept
 {
     return id;
 }
 
-std::string Seller::get_name() const
+std::string Seller::get_name() const noexcept
 {
     return name;
 }
 
-std::string Seller::get_surname() const
+std::string Seller::get_surname() const noexcept
 {
     return surname;
 }
 
-Accessibility Seller::get_accessibility() const
+Accessibility Seller::get_accessibility() const noexcept
 {
     return accessibility;
 }
 
-void Seller::set_name(std::string new_name)
+void Seller::set_name(std::string new_name) noexcept
 {
     name = new_name;
 }
 
-void Seller::set_surname(std::string new_surname)
+void Seller::set_surname(std::string new_surname) noexcept
 {
     surname = new_surname;
 }
 
-void Seller::set_accessibility(Accessibility new_accessibility)
+void Seller::set_accessibility(Accessibility new_accessibility) noexcept
 {
     accessibility = new_accessibility;
+}
+
+bool Seller::operator==(const Seller& second_seller) const
+{
+    if(id == second_seller.get_id())
+    {
+        return true;
+    }
+    return false;
 }
