83b68f4aeed9160bc0c3bc1e99e8e8455d70621d
msadowsk <01178589@pw.edu.pl>
2023-05-17 20:52:44 +0200

    added == operator to client

--- client.cpp
+++ client.cpp
@@ -8,47 +8,56 @@ id(id), name(name), surname(surname), purpose(purpose)
     state = State::waiting;
 }
 
-unsigned int Client::get_id() const
+unsigned int Client::get_id() const noexcept
 {
     return id;
 }
 
-std::string Client::get_name() const
+std::string Client::get_name() const noexcept
 {
     return name;
 }
 
-std::string Client::get_surname() const
+std::string Client::get_surname() const noexcept
 {
     return surname;
 }
 
-State Client::get_state() const
+State Client::get_state() const noexcept
 {
     return state;
 }
 
-Purpose Client::get_purpose() const
+Purpose Client::get_purpose() const noexcept
 {
     return purpose;
 }
 
-void Client::set_name(std::string new_name)
+void Client::set_name(std::string new_name) noexcept
 {
     name = new_name;
 }
 
-void Client::set_surname(std::string new_surname)
+void Client::set_surname(std::string new_surname) noexcept
 {
     surname = new_surname;
 }
 
-void Client::set_state(State new_state)
+void Client::set_state(State new_state) noexcept
 {
     state = new_state;
 }
 
-void Client::set_purpose(Purpose new_purpose)
+void Client::set_purpose(Purpose new_purpose) noexcept
 {
     purpose = new_purpose;
+}
+
+bool Client::operator==(const Client& second_client) const
+{
+    if(id == second_client.get_id())
+    {
+        return true;
+    }
+    return false;
 }
