ab415922d341885f88293e4a2370f75a8e699bdb
msadowsk <01178589@pw.edu.pl>
2023-05-24 21:04:23 +0200

    simulation works for many clients

--- simulation.cpp
+++ simulation.cpp
@@ -18,6 +18,7 @@
 #include <list>
 #include <thread>
 #include <vector>
+#include <memory>
 
 Simulation::Simulation() {}
 
@@ -46,12 +47,14 @@ void Simulation::do_simulation(std::vector<std::string> parameters)
     std::this_thread::sleep_for(std::chrono::seconds(1));
 
     // stworzenie listy obiektów typu sprzedawca
-    //.....
-    // stworzenie listy obiektów typu klient
     std::srand(std::time(nullptr));
-    ClientsList cl;
-    cl.make_list(number_of_clients);
-    cl.print_list();
+    SellersList list_of_sellers;
+    list_of_sellers.make_list(number_of_sellers);
+    list_of_sellers.print_list();
+    // stworzenie listy obiektów typu klient
+    ClientsList list_of_clients;
+    list_of_clients.make_list(number_of_clients);
+    list_of_clients.print_list();
     // std::cout << "\n";
 
     std::string text = "books.txt";
@@ -63,50 +66,89 @@ void Simulation::do_simulation(std::vector<std::string> parameters)
               << std::endl
               << "List of books:\n";
 
-    Bookcollection bc; // initialize book collection
-    bc.make_list_from_file(M);
-    bc.print_list();
+    Bookcollection collection_of_books; // initialize book collection
+    collection_of_books.make_list_from_file(M);
+    collection_of_books.print_list();
 
-    int randomBook_id = 0;
     // int randomAction = 0;
-    unsigned int client_id = 1, x = 1; //, new_client_id; // first client to service
+    //unsigned int client_id = 1, x = 1; //, new_client_id; // first client to service
+    // random book for all clients
+    int randomBook_id = 0;
+    for(auto &client_ptr : list_of_clients.get_clients())
+    {
+        randomBook_id = std::rand() % number_of_books + 1;
+        client_ptr -> set_id_of_book(randomBook_id);
+    }
 
-    randomBook_id = std::rand() % number_of_books;
+    //randomBook_id = std::rand() % number_of_books;
+    int number_of_now_servicing_clients = std::min(number_of_clients, number_of_sellers);
+    // int numbers_of_iteration = number_of_now_servicing_clients;
+    ClientsList now_servicing_clients;
+    ClientsList clients_in_queue;
+    clients_in_queue = now_servicing_clients.pass_client_to_queue(list_of_clients, number_of_now_servicing_clients, list_of_sellers);
+
 
     std::cout << "\nSIMULATION STARTED\n";
     while (time < time_max) // simulation loop
     {
-        std::cout << "TIME[s]: " << time;
-        x = client_id;
-        client_id = cl.return_first_client();
-        // std::cout << " x, client_id = " << x << " " << client_id << "   ";
-        //    new_client_id = cl.return_first_client();
-        if (x == client_id) // servicing the same client which is in previous loop
+        std::cout << "\nTIME[s]: " << time << "\n";
+        for(auto &client_ptr : now_servicing_clients.get_clients())
         {
-            cl.simulation_step(client_id, bc.get_state(randomBook_id));
-            // bc.change_availability(randomBook, randomAction);
+            Book book = collection_of_books.find_book_by_isbn(client_ptr -> get_book_id());
+            std::cout << client_ptr -> get_name() << ' ' << client_ptr -> get_surname() << " with ID: "
+            << client_ptr -> get_id() << ' ' << "Wants to " << client_ptr -> get_activity() << ":\n";
+            std::cout << book.get_author() << ' ' << book.get_title() << "\n";
+            client_ptr -> set_actual_state();
+            if(client_ptr -> get_state() == State::serviced)
+            {
+                Seller seller = list_of_sellers.find_seller_by_id(client_ptr -> get_seller_id());
+                if(client_ptr -> get_purpose() == Purpose::ask)
+                {
+                    seller.answer_question(book);
+                }
+                else
+                {
+                    seller.bill_presentation(book, client_ptr -> get_purpose());
+                }
+                now_servicing_clients.delete_client(client_ptr -> get_id());
+                std::shared_ptr<Client> new_client = clients_in_queue.get_clients().front();
+                new_client -> set_seller(seller.get_id());
+                now_servicing_clients.add_client_as_ptr(new_client);
+            }
+            else std::cout << "\n";
         }
-        else // start servicing the new client so theres need to choose new book
-        {
-            // for (int i = 0; i < number_of_sellers; i++)
-            //{
-            //  Client a = cl.find_client_by_id(client_id);
+            /*
+            x = client_id;
+            client_id = list_of_clients.return_first_client();
+            // std::cout << " x, client_id = " << x << " " << client_id << "   ";
+            //    new_client_id = cl.return_first_client();
+            if (x == client_id) // servicing the same client which is in previous loop
+            {
+                list_of_clients.simulation_step(client_id, collection_of_books.get_state(randomBook_id));
+                // bc.change_availability(randomBook, randomAction);
+            }
+            else // start servicing the new client so theres need to choose new book
+            {
+                // for (int i = 0; i < number_of_sellers; i++)
+                //{
+                //  Client a = cl.find_client_by_id(client_id);
 
-            randomBook_id = std::rand() % number_of_books; // choosing book
-            cl.simulation_step(client_id, bc.get_state(randomBook_id));
-            // bc.change_availability(randomBook, randomAction);
-        }
-        std::cout << " Client nr " << client_id
-                  << " wants to " << cl.activity(client_id) << /* enumToString(a.get_purpose()) <<*/ " book titled: " << bc.print_title(randomBook_id)
-                  << " and obtained response price: " << bc.calculate_book_price(randomBook_id) << " client is: " << cl.get_state(client_id) << " by " << std::endl; // print in terminal simulation results
+                randomBook_id = std::rand() % number_of_books; // choosing book
+                list_of_clients.simulation_step(client_id, collection_of_books.get_state(randomBook_id));
+                // bc.change_availability(randomBook, randomAction);
+            }
+            */
 
-        file << "TIME[s]: " << time << " Client nr " << client_id
-             << " wants to " << cl.activity(client_id) << /* enumToString(a.get_purpose()) <<*/ " book titled: " << bc.print_title(randomBook_id)
-             << " and obtained response price: " << bc.calculate_book_price(randomBook_id) << " client is: " << cl.get_state(client_id) << " by " << std::endl; // save to file simulation result
+            //std::cout << " Client nr " << client_id
+            //        << " wants to " << list_of_clients.activity(client_id) << /* enumToString(a.get_purpose()) <<*/ " book titled: " << collection_of_books.print_title(randomBook_id)
+             //       << " and obtained response price: " << collection_of_books.calculate_book_price(randomBook_id) << " client is: " << list_of_clients.get_state(client_id) << " by " << std::endl; // print in terminal simulation results
 
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+            //file << "TIME[s]: " << time << " Client nr " << client_id
+            //    << " wants to " << list_of_clients.activity(client_id) << /* enumToString(a.get_purpose()) <<*/ " book titled: " << collection_of_books.print_title(randomBook_id)
+             //   << " and obtained response price: " << collection_of_books.calculate_book_price(randomBook_id) << " client is: " << list_of_clients.get_state(client_id) << " by " << std::endl; // save to file simulation result
 
-        // client_id++;
+           // std::this_thread::sleep_for(std::chrono::seconds(1));
+            // client_id++;
         time++;
     }
     // std::cout << "\nEnd of simulation, books status:\n";
