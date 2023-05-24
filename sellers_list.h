aec1aafa9daddd147a8ef0a9a9759d52744042d7
msadowsk <01178589@pw.edu.pl>
2023-05-24 21:05:52 +0200

    added print and make from data

--- sellers_list.h
+++ sellers_list.h
@@ -1,5 +1,6 @@
 #ifndef SELLERS_LIST_H
 #define SELLERS_LIST_H
+#include "sellers_list.h"
 #include "seller.h"
 #include <list>
 #include <memory>
@@ -13,6 +14,10 @@ class SellersList
         void add_seller(Seller seller);
         std::list<std::shared_ptr<Seller>> get_sellers();
         Seller find_seller_by_id(unsigned int id) const;
+        void make_list(unsigned int number_of_sellers);
+        void print_list() noexcept;
+        void simulation_step();
 };
 
 #endif
+
