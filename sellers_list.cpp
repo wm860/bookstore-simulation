f05abbeeb40c81a5e7501a9052bb4418c64e988a
msadowsk <01178589@pw.edu.pl>
2023-05-24 21:06:25 +0200

    added make from data

--- sellers_list.cpp
+++ sellers_list.cpp
@@ -1,6 +1,7 @@
 #include "sellers_list.h"
 #include "seller_not_found_exception.h"
 #include "already_existing_seller_exception.h"
+#include <iostream>
 
 void SellersList::add_seller(Seller seller)
 {
@@ -31,3 +32,36 @@ std::list<std::shared_ptr<Seller>> SellersList::get_sellers()
 {
     return sellers;
 }
+
+void SellersList::make_list(unsigned int number_of_sellers)
+{
+    for(unsigned int i = 0; i < number_of_sellers; i++)
+    {
+        unsigned int id = i + 1;
+        std::string name = "Name" + std::to_string(id);
+        std::string surname = "Surname" + std::to_string(id);
+        Accessibility accessibility = Accessibility::accessible;
+        Seller seller(id, name, surname, accessibility);
+        add_seller(seller);
+    }
+}
+
+void SellersList::print_list() noexcept
+{
+    std::cout << "\nSellers list:\n";
+    for(const auto &seller_ptr : sellers)
+    {
+        std::cout << "Seller ID: " << seller_ptr -> get_id() << "\n";
+        std::cout << "Name: " << seller_ptr -> get_name() << "\n";
+        std::cout << "Surname: " << seller_ptr -> get_surname() << "\n\n";
+    }
+}
+/*
+void SellersList::simulation_step()
+{
+    for(const auto &seller_ptr : sellers)
+    {
+
+    }
+}
+*/
